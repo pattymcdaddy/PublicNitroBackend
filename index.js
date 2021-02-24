1693c1fcbc3d9e9695cda7647ef0b4a6a96f6e11
ptrcko2 <61922326+ptrcko2@users.noreply.github.com>
2021-02-24 18:55:35 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+app.use(express.static("public"));
+const fs = require('fs');
+require('dotenv').config();
+const token = process.env.token;
+app.get("/", (req, res) => {
+  return res.json({"error": "you bumped into an error! please make sure you have the URL set correctly."})
+});
+
+
+
+app.get("/v1/", (req, res) => {
+    let guild = req.query.guild;
+    let id = req.query.id;
+    if (!guild) return res.json({"isBooster": false})
+    if (!id) return res.json({"isBooster": false})
+    client.guilds.cache.get(guild).members.fetch(id).then(m => {
+        if(m.premiumSince === null) return res.json({"isBooster": false})
+        
+        
+        
+        if(m.premiumSince) return res.json({"isBooster": true})
+        
+        })
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+  let botstatus = fs.readFileSync('./bot-status.json');
+  botstatus = JSON.parse(botstatus);
+  if(botstatus.activity == 'false') return;
+  if(botstatus.activitytype == 'STREAMING'){
+    client.user.setActivity(botstatus.activitytext, {
+      type: botstatus.activitytype,
+      url: botstatus.activityurl
+    });
+  } else {
+    client.user.setActivity(botstatus.activitytext, {
+      type: botstatus.activitytype
+    });
+  }
+ });
+
+client.login(token);
